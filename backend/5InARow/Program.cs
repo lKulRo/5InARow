d75f69cfe9655b495e9c0305e7921c0168a303d9
Tran Trung <38261549+lKulRo@users.noreply.github.com>
2024-05-02 22:14:38 +0200

    Update Program.cs

--- backend/5InARow/Program.cs
+++ backend/5InARow/Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
 }
 app.UseCors(x => x
         .WithOrigins("http://localhost:8080")
-        .WithOrigins("http://49.13.164.181")
+        .WithOrigins("http://49.13.164.181:8080")
         .WithMethods("POST", "GET")
         .AllowAnyHeader()
         .AllowCredentials());
